56107e27063114d114cb8fa812f6d066fc50383f
Berkay KARABAS <berkay.karabas091@gmail.com>
2023-08-11 16:54:57 +0300

    ceaser cryptograph

---
+++ main.py
@@ -0,0 +1,20 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def ceaser(plain_text, shift_amount, direct):
+    end_text = ""
+    if direct == "decode":
+        shift_amount *= -1
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        end_text += alphabet[new_position]
+    print(f"Here's the {direction}d result: {end_text}")
+
+
+ceaser(plain_text=text, shift_amount=shift, direct=direction)
